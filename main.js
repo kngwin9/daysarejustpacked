c4bbd2f7f30cdf15cecf00d1a11f7e9d8eaad915
Kevin Nguyen <kngwin9@gmail.com>
2017-03-13 15:10:52 -0700

    Added color display, will need to add player 1 display when game starts

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function click_handler() {
     $("#grid_board").on('click' , 'div' , function(){
         game.createPlayers();
         game.engine($(this).attr("value"));
-        game.playerSwitch();
+        game.playerSwitch(); // Switches player on click, could move later to something else.
     });
 
 }
@@ -36,7 +36,7 @@ function playerFactory(symbol,playerTurn){
     };
     this.nonActivePlayer = function(){
         this.playerTurn.removeClass('current_player');
-    }
+    };
 }
 
 function gameConstructor() {
@@ -53,16 +53,20 @@ function gameConstructor() {
         this.players.push(player2);
         this.players[0].activePlayer();                      //this makes player one the active player as soon as it's created
     };
-    this.playerSwitch = function () {
+    this.playerSwitch = function () { // Switches player from Player 1 to Player 2 and also displays whose turn it is.
         if(this.current_player) {
             this.current_player = 0;
+            this.players[0].activePlayer();
+            this.players[1].nonActivePlayer();
             console.log("Current player is: " + this.current_player)
         }
         else {
             this.current_player = 1;
+            this.players[1].activePlayer();
+            this.players[0].nonActivePlayer();
             console.log("Current player is: " + this.current_player)
         }
-    }
+    };
 }
 
 // Grid template for color selector and player change.
