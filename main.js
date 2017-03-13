54f19f871a44414c4b0c7193574fb48ec8898147
Kevin Nguyen <kngwin9@gmail.com>
2017-03-13 11:01:30 -0700

    Added firebase conifguration

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 /**
  * Created by kngwin9 on 3/13/17.
  */
+
+// Firebase Config
+var config = {
+    apiKey: "AIzaSyBnHoatRyvoF6KJAgF2rI95MpCemMdO_CY",
+    authDomain: "tictactoe-b9ace.firebaseapp.com",
+    databaseURL: "https://tictactoe-b9ace.firebaseio.com",
+    storageBucket: "tictactoe-b9ace.appspot.com",
+    messagingSenderId: "51570292060"
+};
+firebase.initializeApp(config);
