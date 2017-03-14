4bac372a06e71be2aa8b7d952f132d9d29dd0620
Kevin Nguyen <kngwin9@gmail.com>
2017-03-14 09:58:01 -0700

    Merging test branch to main v0.3

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-/**
- * Created by kngwin9 on 3/13/17.
- */
-// Firebase Config
 var config = {
     apiKey: "AIzaSyBnHoatRyvoF6KJAgF2rI95MpCemMdO_CY",
     authDomain: "tictactoe-b9ace.firebaseapp.com",
@@ -13,101 +9,83 @@ firebase.initializeApp(config);
 
 var game = new gameConstructor();
 var display = new displayConstructor();
-
-
+var player = new playerConstructor();
 $(document).ready(function () {
+    player.create();
     click_handler();
-   // game.playerStart();
-    $('#player1').addClass('current_player');
 });
 function click_handler() {
-    console.log("Click Handler Works");
     $("#grid_board").on('click' , 'div' , function(){
-        game.createPlayers();
-        display.playerSymbol(this, game.counter % 2 === 0 ? "O" : "X");
-        game.engine($(this).attr("value"));
-        game.playerSwitch(); // Switches player on click, could move later to something else.
-        game.playerLock();
+        display.playerSymbol(this, player.currentCharacter.symbol);
+        game.markArray($(this).attr("value"));
+        player.switchPlayer();
     });
 }
-function playerFactory(symbol,playerTurn){
-    this.symbol = symbol;
-    this.playerTurn = playerTurn;
-    this.symbol = function(){  //returns the symbol for the particular player created
-        return this.symbol;
-    };
-    this.activePlayer = function(){
-        this.playerTurn.addClass('current_player');
-    };
-    this.nonActivePlayer = function(){
-        this.playerTurn.removeClass('current_player');
-    };
-}
+
 function gameConstructor() {
-    console.log("Game constructor works!");
-    this.winningPattern = [
+    this.board = [];
+    this.winCombination = [
         [0,1,2],
-        [0,4,8],
+        [3,4,5],
+        [6,7,8],
         [0,3,6],
         [1,4,7],
-        [2,4,6],
         [2,5,8],
-        [3,4,5],
-        [6,7,8]
+        [0,4,8],
+        [2,4,6]
     ];
-    this.players = [];
-    this.counter = 1;
-    this.engine = function (userPositionInput) {
-    game.counter++;
+    this.markArray = function (cellPosition) {
+        this.board[cellPosition] = player.currentCharacter.symbol;
+        console.log(this.board);
+        this.winCheck();
     };
-
-    /*
-    this.playerStart = function() {
-        console.log(this.players);
-        this.start = this.players.sort(function (a, b) {
-            return 0.5 - math.random()
-        });
-    };
-
-    */
-
-    this.createPlayers = function(){                          //will call on player factory
-        var player1 =  new playerFactory('X',$('#player1'));
-        var player2 = new playerFactory('O',$('#player2'));
-        this.players.push(player1);
-        this.players.push(player2);
-        this.players[0].activePlayer();                      //this makes player one the active player as soon as it's created
-    };
-    this.playerSwitch = function () { // Switches player from Player 1 to Player 2 and also displays whose turn it is.
-        if(this.current_player) {
-            this.current_player = 0;
-            this.players[0].activePlayer();
-            this.players[1].nonActivePlayer();
-            console.log("Current player is: " + this.current_player)
-        }
-        else {
-            this.current_player = 1;
-            this.players[1].activePlayer();
-            this.players[0].nonActivePlayer();
-            console.log("Current player is: " + this.current_player)
-        }
-    };
-
-    /*
-    this.playerLock = function () { // Deactivates the non-current player, prevents other player from clicking on things
-        if(this.current_player) {
-
-        }
-        else {
-
+    this.winCheck = function () {
+        for(i = 0; i  < this.winCombination.length; i++){
+            if(this.board[this.winCombination[i][0]] === player.currentCharacter.symbol && this.board[this.winCombination[i][1]] === player.currentCharacter.symbol && this.board[this.winCombination[i][2]] === player.currentCharacter.symbol){
+                alert("Player " + player.currentCharacter.symbol + " wins");
+            }
         }
     }
-    */
+}
+function playerConstructor() {
+    //this.currentPlayer = null;
+    this.create = function () {
+        this.character = [
+            {element: $("#player1").addClass('current_player')},
+            {element: $("#player2")}
+        ];
+        this.character = this.character.sort(function () {
+           return 0.5 - Math.random()
+        });
+        console.log(this.character);
+
+
+        this.character[0].symbol = this.character[0].element.text();
+        this.character[1].symbol = this.character[1].element.text();
+        this.currentCharacter = this.character[0];
+    };
+    this.activate = function (playerToActiv) {
+        playerToActiv.element.addClass('current_player');
+        this.currentCharacter = playerToActiv;
+    };
+    this.deactivate = function (playerToDeact) {
+        playerToDeact.element.removeClass('current_player');
+       // $("#grid_board").off('click' , 'div');
+    };
+    this.switchPlayer = function () {
+        if(this.currentCharacter === this.character[0]) {
+            this.deactivate(this.character[0]);
+            this.activate(this.character[1]);
+            console.log("Current player is: " + this.currentCharacter.symbol);
+        } else {
+            this.deactivate(this.character[1]);
+            this.activate(this.character[0]);
+            console.log("Current player is: " + this.currentCharacter.symbol);
+        }
+    };
 }
 function displayConstructor() {
-    this.playerSymbol = function (location,symbol) {
-        console.log("working");
+    this.playerSymbol = function (location, symbol) {
         $(location).text(symbol);
     }
 }
-
